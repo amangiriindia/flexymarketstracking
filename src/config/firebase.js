0d59ffd436f91e26fa4bb6c249c3ebae925a6b4a
Aman Giri <97629888+amangiriindia@users.noreply.github.com>
2026-02-07 13:37:48 +0530

    make auto regisation if user not exist

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,7 +1,7 @@
 // src/config/firebase.js
 
 const admin = require('firebase-admin');
-const serviceAccount = require('./flexy-66d49-firebase-adminsdk-fbsvc-3660285967.json');
+const serviceAccount = require('./flexy-66d49-firebase-adminsdk-fbsvc-843e6f818e.json');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
